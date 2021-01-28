30488df745912aac695f57364d8009d99b16cdcf
ronenkapelian <72082238+ronenkapelian@users.noreply.github.com>
2021-01-28 16:31:54 +0200

    Add ci (#10)
    * Create python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * feat(automation):add version mark for CI process - generate_version.sh
    
    * Update generate_version.sh
    
    * Update python-publish.yml
    
    * Update generate_version.sh
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * added set env to generate_version script
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * removed set version its already a variable
    
    * removed set version its already a variable
    
    * changed ecr repo name
    
    * changed version in req file
    
    * Update python-publish.yml
    
    * Update python-publish.yml
    
    * Update Dockerfile
    
    * Update python-publish.yml
    
    * Update Dockerfile
    
    * changed to build on push, cleaned up dockerfile, removed version from requirements file, fixed sh syntax
    
    Co-authored-by: yossiz16 <yossiz16@gmail.com>
    Co-authored-by: Danny Tuaev <daniel.tuaev@gmail.com>
    Co-authored-by: dtuaev <50065761+tech27@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-
 export FILE_LOGS=1 # remove to avoid logging to file - will print only to console
-
-
-
-if [[ $CLEAN_UP == "1" ]]
+if [ "$CLEAN_UP" = true ]
 then
-    echo "clean up mode"
+    echo "<<<<____CLEAN UP IN PROGRESS____>>>>"
     python /source_code/server_automation/tests/cleanup.py
-
 else
-    echo "test mode"
+    echo "####____ RUNNING IN TEST MODE ____####"
     export OUTPUT_EXPORT_PATH=/opt/output
     export LOGS_OUTPUT=/opt/logs
     pytest /source_code/server_automation/tests
