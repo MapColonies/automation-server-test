d6af4c17cedd5a2497fa71cb68ba65a56cbd550f
yossiz16 <yossiz16@gmail.com>
2021-02-18 13:12:11 +0200

    Update generate_version.sh

--- generate_version.sh
+++ generate_version.sh
@@ -2,11 +2,12 @@
 pip3 install setuptools setuptools_scm
 VER=$(python3 setup.py --version | sed 's/+/./g')
 
+export VERSION=$(python3 setup.py --version | sed 's/+/./g')
+
 echo $(date):automation-test:$VER
 
 echo version=$VER>version.txt
 echo version=$VER>>version_history.txt
-export version=$VER
 echo $version
 echo "::set-env name=TAG_NAME::$VER"
 
