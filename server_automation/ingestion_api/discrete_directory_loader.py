078c3064f17ed10dac35682d306d388d1eac27e1
ronenk1 <ronenk1@rnd-hub.com>
2021-07-18 17:02:46 +0300

    chore(automation): adding shape convertor to json ingestion

--- server_automation/ingestion_api/discrete_directory_loader.py
+++ server_automation/ingestion_api/discrete_directory_loader.py
@@ -1,6 +1,7 @@
 """This module responsible interface with orig upload directory"""
 import logging
 import os
+from mc_automation_tools import shape_convertor as sv
 from server_automation.configuration import config_ingestion
 _log = logging.getLogger('server_automation.ingestion_api.discrete_directory_loader')
 
@@ -24,10 +25,11 @@ def validate_source_directory(path):
         _log.error(f'Path [{os.path.join(path, config_ingestion.SHAPES_PATH)}] missing files:\n'
                    f'{set(config_ingestion.SHAPE_FILE_LIST).symmetric_difference(set(config_ingestion.SHAPE_FILE_LIST).intersection(os.listdir(os.path.join(path, config_ingestion.SHAPES_PATH))))}')
         return False
-    # if not all(item in os.listdir(os.path.join(path, config_ingestion.SHAPES_PATH)) for item in
-    #     config_ingestion.SHAPE_FILE_LIST):
-    #     _log.error(f'Path [{os.path.join(path, config_ingestion.SHAPES_PATH)}] not included relevant files:\n'
-    #                f'requested files: {config_ingestion.SHAPE_FILE_LIST}')
-    #     return False
+
+    shape_metadata_shp = sv.shp_to_geojson(os.path.join(path, config_ingestion.SHAPES_PATH, 'ShapeMetadata.shp'))
+    Product_shp = sv.shp_to_geojson(os.path.join(path, config_ingestion.SHAPES_PATH, 'Product.shp'))
+    Files_shp = sv.shp_to_geojson(os.path.join(path, config_ingestion.SHAPES_PATH, 'Files.shp'))
+    img_path = os.path.join(path, config_ingestion.TIFF_PATH)
+    sv.generate_oversear_request(shape_metadata_shp, Product_shp, Files_shp, img_path)
 
     print("on progress")
